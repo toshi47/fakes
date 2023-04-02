1139ccc0c89e5b4049589ddeb4ee9856e4f87d7d
Aleksandr Lyrchikov <a.lyrchikov@yadro.com>
2023-04-02 21:52:51 +0300

    FD-2 init skeleton

---
+++ api/main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Print("kek")
+}
+
+// TODO: api to auth
+
+// TODO: api to check if text is fake (mb +link)
+// POST /check_news
+
+// TODO: api to check if image is fake
+// POST /check_image
