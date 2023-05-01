82a270d1a2cc3e0c09f4606966d51dda1a1adffe
Aleksandr Lyrchikov <a.lyrchikov@yadro.com>
2023-05-01 20:55:55 +0300

    FD-12 added nginx reverse proxy, configured ssl

--- deploy.sh
+++ deploy.sh
@@ -8,11 +8,11 @@ fi
 
 echo deploying to $addr ...
 echo copying files...
-rsync -av . root@$addr:/fakes
+rsync -av --delete-after . root@$addr:/fakes
 echo starting docker-compose...
 ssh root@$addr << EOF
     cd /fakes
     docker-compose stop
     docker-compose up --build --force-recreate -d
-    docker image prune -y
+    docker image prune -f
 EOF
